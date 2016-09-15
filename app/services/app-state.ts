6da8bc8d0ef439c0c0026ced7da0ddc1c01c5046
unknown <s.cunningham777@gmail.com>
2016-09-14 21:30:26 -0500

    Adding active task list to AppState

--- app/services/app-state.ts
+++ app/services/app-state.ts
@@ -1,5 +1,7 @@
 import {ICharacter} from '../models/character-types';
+import {ITask} from '../models/task-types';
 
 export interface AppState {
 	curCharacter: ICharacter;
+	activeTasks: ITask[];
 }
