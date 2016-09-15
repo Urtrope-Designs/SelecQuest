68cab47ea71ebaf0207b10da5470162d3e53388e
unknown <s.cunningham777@gmail.com>
2016-09-14 23:41:04 -0500

    TDDing TaskQueueingService

---
+++ app/services/task-queueing-service.spec.ts
@@ -0,0 +1,57 @@
+/// <reference path="../../typings/jasmine.d.ts" />
+import {provide} from '@angular/core';
+import {beforeEachProviders, it, describe, expect, inject} from '@angular/core/testing';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
+import {TaskQueueingService} from './task-queueing-service';
+import {TaskActions} from '../actions/task-actions';
+
+
+describe('TaskQueueingService', () => {
+
+	let selectStub;
+	class StoreMock {
+		constructor() {
+			selectStub = spyOn(this, 'select').and.callThrough();
+		}
+		select() {
+			return Observable.empty();
+		}
+	}
+
+	let addTaskStub;
+	class TaskActionsMock {
+		constructor() {
+			addTaskStub = spyOn(this, 'addTask');
+		}
+		addTask() {}
+	}
+
+	let generateTaskStub;
+	let fakeTask = {
+		name: 'Fake Task'
+	}
+	class TaskFactoryMock {
+		constructor() {
+			generateTaskStub = spyOn(this, 'generateTask').and.callThrough();
+		}
+		generateTask() {
+			return fakeTask;
+		}
+	}
+
+	beforeEachProviders(() => [
+			TaskQueueingService,
+			provide(Store, {useClass: StoreMock}),
+			provide(TaskActions, {useClass: TaskActionsMock})
+		]);
+
+	afterEach(() => {
+		selectStub.calls.reset();
+		addTaskStub.calls.reset();
+	})
+
+	it('should add a new task if no active tasks exist in store.activeTasks', inject([TaskQueueingService], (taskSvc: TaskQueueingService) => {
+
+	}));
+});
