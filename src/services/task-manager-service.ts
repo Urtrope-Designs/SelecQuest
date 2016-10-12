e320fb127516e170a9c0cc6c9eb8ba15cea5ea8c
Sean Cunningham <seancunningham@Margause.local>
2016-10-12 01:36:34 -0500

    Work on task managing observables.

--- src/services/task-manager-service.ts
+++ src/services/task-manager-service.ts
@@ -1,10 +1,14 @@
 import {Injectable} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/distinctKey';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/observable/combineLatest';
 import {AppState} from './app-state';
 import {ITask} from '../models/task-types';
 import {TaskFactoryService} from './task-factory-service';
 import {TaskActions} from '../actions/task-actions';
+import {ICharacter} from '../models/character-types';
 
 @Injectable()
 export class TaskManagerService{
@@ -17,20 +21,35 @@ export class TaskManagerService{
         public taskActions: TaskActions
     ) {
         let task$ = this.store.select('activeTasks');
-        let newCharacter$ = this.store.select('curCharacter').distinctKey('id');
-        this.taskCheck$ = Observable.combineLatest(newCharacter$, task$);
-        this.task$.subscribe((character: ICharacter, tasks: ITask[]) => {
-            console.log('character:');
-            console.dir(character);
-            console.log('tasks:');
-            console.dir(tasks);
-            //TODO: get the numActiveTasksAllowed from current character?
-            if (tasks.length < this.numActiveTasksAllowed) {
-                //TODO: pull the task type from current character settings
-                let newTask = this.taskFactory.generateTask(null);
-                this.store.dispatch(this.taskActions.addTask(newTask));
-            }
-        })
+        // let newCharacter$ = 
+        this.store.select('curCharacter')
+            .filter((character: ICharacter) => {
+                return (!!character);
+            })
+            .distinctKey('id')
+            .do((character: ICharacter) => {
+                //dispatch event to clear any reward buffer not for this character's id
+                //dispatch event to clear any task not for this character's id
+            })
+            .subscribe((character: ICharacter) => {
+                //clear out old subscription
+                //subscribe to interval to check on current task
+            })
+
+        // this.taskCheck$ = Observable.combineLatest(newCharacter$, task$);
+        // this.taskCheck$.subscribe((latest: [ICharacter, ITask[]]) => {
+        //     var [character, tasks] = latest;
+        //     console.log('character:');
+        //     console.dir(character);
+        //     console.log('tasks:');
+        //     console.dir(tasks);
+        //     //TODO: get the numActiveTasksAllowed from current character?
+        //     if (tasks.length < this.numActiveTasksAllowed) {
+        //         //TODO: pull the task type from current character settings
+        //         let newTask = this.taskFactory.generateTask(null);
+        //         this.store.dispatch(this.taskActions.addTask(newTask));
+        //     }
+        // });
     }
 
 }
