3f22a20efe67440a43690f95f4be29b6dab02414
s.cunningham777@gmail.com <s.cunningham777@gmail.com>
2018-06-29 23:55:27 -0500

    UI conversion to tables more or less complete.

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -75,104 +75,136 @@ export class AppHome {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr><td sq-mr-auto>Str</td><td>{this.character.str}</td></tr>
-                            <tr><td sq-mr-auto>Dex</td><td>{this.character.dex}</td></tr>
-                            <tr><td sq-mr-auto>Con</td><td>{this.character.con}</td></tr>
-                            <tr><td sq-mr-auto>Wis</td><td>{this.character.wis}</td></tr>
-                            <tr><td sq-mr-auto>Int</td><td>{this.character.int}</td></tr>
-                            <tr><td sq-mr-auto>Cha</td><td>{this.character.cha}</td></tr>
-                            <tr><td sq-mr-auto>Max HP</td><td>{this.character.maxHp}</td></tr>
-                            <tr><td sq-mr-auto>Max MP</td><td>{this.character.maxMp}</td></tr>
+                            <tr><td>Str</td><td>{this.character.str}</td></tr>
+                            <tr><td>Dex</td><td>{this.character.dex}</td></tr>
+                            <tr><td>Con</td><td>{this.character.con}</td></tr>
+                            <tr><td>Wis</td><td>{this.character.wis}</td></tr>
+                            <tr><td>Int</td><td>{this.character.int}</td></tr>
+                            <tr><td>Cha</td><td>{this.character.cha}</td></tr>
+                            <tr><td>Max HP</td><td>{this.character.maxHp}</td></tr>
+                            <tr><td>Max MP</td><td>{this.character.maxMp}</td></tr>
                         </tbody>
                     </table>
                     <p>
                         <div sq-flex class="textRow"><span sq-mr-auto>XP to next level</span> {getXpRequiredForNextLevel(this.character.level) - this.character.currentXp}</div>
                     </p>
-                    <p class="listBox">
-                        <div sq-flex class="item-highlight textRow">
-                            <span sq-mr-auto>Spells</span>
-                            <span>Rank</span>
-                        </div>
-                        {
-                            this.character.spells.length == 0 
-                            ? <div class="textRow">[None]</div>    
-                            : this.character.spells.map((spell) => 
-                                    <div sq-flex class="textRow"><span sq-mr-auto>{spell.name}</span> {spell.rank}</div>
-                                )
-                        }
-                    </p>
-                    <p class="listBox">
-                        <div sq-flex class="item-highlight textRow">
-                            <span sq-mr-auto>Abilities</span>
-                            <span>Rank</span>
-                        </div>
-                        {
-                            this.character.abilities.length == 0 
-                            ? <div class="textRow">[None]</div>    
-                            : this.character.abilities.map((ability) => 
-                                    <div sq-flex class="textRow"><span sq-mr-auto>{ability.name}</span> {ability.rank}</div>
-                                )
-                        }
-                    </p>
+                    <table class="listBox">
+                        <thead>
+                            <tr>
+                                <th style={{width: "65%"}}>Spells</th>
+                                <th>Rank</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.character.spells.length == 0 
+                                ? <tr><td colSpan={2}>[None]</td></tr>    
+                                : this.character.spells.map((spell) => 
+                                        <tr>
+                                            <td>{spell.name}</td>
+                                            <td>{spell.rank}</td>
+                                        </tr>
+                                    )
+                            }
+                        </tbody>
+                    </table>
+                    <table class="listBox">
+                        <thead>
+                            <tr>
+                                <th style={{width: "65%"}}>Abilities</th>
+                                <th>Rank</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.character.abilities.length == 0 
+                                ? <tr><td colSpan={2}>[None]</td></tr>    
+                                : this.character.abilities.map((ability) => 
+                                        <tr>
+                                            <td>{ability.name}</td>
+                                            <td>{ability.rank}</td>
+                                        </tr>
+                                    )
+                            }
+                        </tbody>
+                    </table>
                     <p>
                         <div sq-flex class="textRow"><span sq-mr-auto>Gold</span> {this.character.gold}</div>
                         <div sq-flex class="textRow">
                             <span sq-mr-auto>Market Saturation</span> {this.character.marketSaturation} / {this.character.maxMarketSaturation}
                         </div>
                         <div sq-flex class="textRow"><span sq-mr-auto>Encumbrance</span> {this.character.loot.reduce((prevVal, curItem) => {return prevVal + curItem.quantity}, 0)} / {this.character.maxEncumbrance}</div>
-                        <div class="listBox">
-                            <div sq-flex class="item-highlight textRow">
-                                <span sq-mr-auto>Loot</span>
-                                <span>Qty</span>
-                            </div>
+                    </p>
+                    <table class="listBox">
+                        <thead>
+                            <tr>
+                                <th style={{width: "65%"}}>Loot</th>
+                                <th>Qty</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             {
                                 this.character.loot.length == 0
-                                ? <div class="textRow">[None]</div>
+                                ? <tr><td colSpan={2}>[None]</td></tr>
                                 : this.character.loot.map((item) => 
-                                        <div sq-flex class="textRow"><span sq-mr-auto>{item.name}</span> {item.quantity}</div>
+                                        <tr>
+                                            <td>{item.name}</td>
+                                            <td>{item.quantity}</td>
+                                        </tr>
                                     )
                             }
-                        </div>
-                    </p>
+                        </tbody>
+                    </table>
                     <p>
                         <div sq-flex class="textRow"><span sq-mr-auto>Renown</span> {this.character.renown}</div>
                         <div sq-flex class="textRow">
                             <span sq-mr-auto>Fatigue</span> {this.character.fatigue} / {this.character.maxFatigue}
                         </div>
                         <div sq-flex class="textRow"><span sq-mr-auto>Equipment Wear</span> {this.character.trophies.reduce((prevVal, curItem) => {return prevVal + curItem.quantity}, 0)} / {this.character.maxEquipmentWear}</div>
-                        <div class="listBox">
-                            <div sq-flex class="item-highlight textRow">
-                                <span sq-mr-auto>Trophies</span>
-                                <span>Qty</span>
-                            </div>
+                    </p>    
+                    <table>
+                        <thead>
+                            <tr>
+                                <td style={{width: "65%"}}>Trophies</td>
+                                <td>Qty</td>
+                            </tr>
+                        </thead>
+                        <tbody>
                             {
                                 this.character.trophies.length == 0
-                                ? <div class="textRow">[None]</div>
+                                ? <tr><td colSpan={2}>[None]</td></tr>
                                 : this.character.trophies.map((item) => 
-                                    <div sq-flex class="textRow"><span sq-mr-auto>{item.name}</span> {item.quantity}</div>
+                                    <tr>
+                                        <td>{item.name}</td>
+                                        <td>{item.quantity}</td>
+                                    </tr>
                                 )
                             }
-                        </div>
-                    </p>
+                        </tbody>
+                    </table>
                     <p>
                         <div sq-flex class="textRow"><span sq-mr-auto>Reputation</span> {this.character.reputation}</div>
                         <div sq-flex class="textRow">
                             <span sq-mr-auto>Social Exposure</span> {this.character.socialExposure} / {this.character.maxSocialCapital}
                         </div>
                         <div sq-flex class="textRow"><span sq-mr-auto>Questlog</span> {this.character.leads.length} / {this.character.maxQuestLogSize}</div>
-                        <div class="listBox">
-                            <div sq-flex class="item-highlight textRow">
-                                <span sq-mr-auto>Quests</span>
-                            </div>
+                    </p>
+                    <table class="listBox">
+                        <thead>
+                            <tr>
+                                <td>Quests</td>
+                            </tr>
+                        </thead>
+                        <tbody>
                             {
                                 this.character.leads.length == 0
-                                ? <div class="textRow">[None]</div>
+                                ? <tr><td>[None]</td></tr>
                                 : this.character.leads.map((item) => 
-                                        <div class="textRow">{item.questlogName}</div>
+                                        <tr><td>{item.questlogName}</td></tr>
                                     )
                             }
-                        </div>
-                    </p>
+                        </tbody>
+                    </table>
                     <table class="listBox">
                         <thead>
                             <tr>
@@ -195,64 +227,78 @@ export class AppHome {
                             }
                         </tbody>
                     </table>
-                    <p class="listBox">
-                        <div sq-flex class="item-highlight textRow">Accolades</div>
-                        {
-                            this.character.accolades.map(accolade =>
-                                <div sq-flex class="textRow">
-                                    <span sq-mr-auto>{AccoladeType[accolade.type]}</span>&nbsp;
-                                    {
-                                        accolade.received.length <= 0
-                                        ? <span>[None]</span>
-                                        : <span>{accolade.received.join(', ')}</span>
-                                    }
-                                </div>
-                            )
-                        }
-                    </p>
-                    <p class="listBox">
-                        <div sq-flex class="item-highlight textRow">Affiliations</div>
-                        <div sq-flex class="textRow">
-                            <span sq-mr-auto>{AffiliationType.CONNECTIONS}</span>&nbsp;
+                    <table class="listBox">
+                        <thead>
+                            <tr>
+                                <th style={{width: "40%"}}>Accolades</th>
+                                <th></th>    
+                            </tr>
+                        </thead>
+                        <tbody>
                             {
-                                this.character.affiliations[AffiliationType.CONNECTIONS].length <= 0
-                                ? <span>[None]</span>
-                                : <span>{
-                                    this.character.affiliations[AffiliationType.CONNECTIONS]
-                                        .map((connection: CharConnection) => {
-                                            return `${connection.affiliatedPersonName}, ${connection.affiliatedPersonTitle} for ${connection.affiliatedGroupName}`;
-                                        })
-                                        .join(', ')
-                                }</span>
+                                this.character.accolades.map(accolade =>
+                                    <tr>
+                                        <td>{AccoladeType[accolade.type]}</td>
+                                        {
+                                            accolade.received.length <= 0
+                                            ? <td>[None]</td>
+                                            : <td>{accolade.received.join(', ')}</td>
+                                        }
+                                    </tr>
+                                )
                             }
-                        </div>
-                        <div sq-flex class="textRow">
-                            <span sq-mr-auto>{AffiliationType.MEMBERSHIPS}</span>&nbsp;
-                            {
-                                this.character.affiliations[AffiliationType.MEMBERSHIPS].length <= 0
-                                ? <span>[None]</span>
-                                : <span>{
-                                    this.character.affiliations[AffiliationType.MEMBERSHIPS]
-                                        .map((membership: CharMembership) => membership.affiliatedGroupName)
-                                        .join(', ')
-                                }</span>
-                            }
-                        </div>
-                        <div sq-flex class="textRow">
-                            <span sq-mr-auto>{AffiliationType.OFFICES}</span>&nbsp;
-                            {
-                                this.character.affiliations[AffiliationType.OFFICES].length <= 0
-                                ? <span>[None]</span>
-                                : <span>{
-                                    this.character.affiliations[AffiliationType.OFFICES]
-                                        .map((office: CharOffice) => {
-                                            return `${office.officeTitleDescription} for ${office.affiliatedGroupName}`;
-                                        })
-                                        .join(', ')
-                                }</span>
-                            }
-                        </div>
-                    </p>
+                        </tbody>
+                    </table>
+                    <table class="listBox">
+                        <thead>
+                            <tr>
+                                <th style={{width: "40%"}}>Affiliations</th>
+                                <th></th>    
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{AffiliationType.CONNECTIONS}</td>
+                                {
+                                    this.character.affiliations[AffiliationType.CONNECTIONS].length <= 0
+                                    ? <td>[None]</td>
+                                    : <td>{
+                                        this.character.affiliations[AffiliationType.CONNECTIONS]
+                                            .map((connection: CharConnection) => {
+                                                return `${connection.affiliatedPersonName}, ${connection.affiliatedPersonTitle} for ${connection.affiliatedGroupName}`;
+                                            })
+                                            .join(', ')
+                                    }</td>
+                                }
+                            </tr>
+                            <tr>
+                                <td>{AffiliationType.MEMBERSHIPS}</td>
+                                {
+                                    this.character.affiliations[AffiliationType.MEMBERSHIPS].length <= 0
+                                    ? <td>[None]</td>
+                                    : <td>{
+                                        this.character.affiliations[AffiliationType.MEMBERSHIPS]
+                                            .map((membership: CharMembership) => membership.affiliatedGroupName)
+                                            .join(', ')
+                                    }</td>
+                                }
+                            </tr>
+                            <tr>
+                                <td>{AffiliationType.OFFICES}</td>
+                                {
+                                    this.character.affiliations[AffiliationType.OFFICES].length <= 0
+                                    ? <td>[None]</td>
+                                    : <td>{
+                                        this.character.affiliations[AffiliationType.OFFICES]
+                                            .map((office: CharOffice) => {
+                                                return `${office.officeTitleDescription} for ${office.affiliatedGroupName}`;
+                                            })
+                                            .join(', ')
+                                    }</td>
+                                }
+                            </tr>
+                        </tbody>
+                    </table>
                     <p>
                         <div sq-flex class="textRow"><span sq-mr-auto>Current Adventure</span> {this.character.currentAdventure.name}</div>
                         <div sq-flex class="textRow"><span sq-mr-auto>Adventure Progress</span> {this.character.adventureProgress} / {this.character.currentAdventure.progressRequired}</div>
